8da52dc7c6bb314c0ecea01b56b9609b41a6ba4c
Webmin <webmin@rx24.ru>
2016-07-12 19:39:20 +0500

    ??????? ???????

---
+++ backend/views/layouts/header.php
@@ -0,0 +1,36 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\Dropdown;
+?>
+
+<header class="main-header">
+
+    <?= Html::a('<span class="logo-mini">ССП</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <nav class="navbar navbar-static-top" role="navigation">
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">
+                    <span class="hidden-xs"><?=Yii::$app->user->identity->username?></span>
+                    <b class="caret"></b></a>
+                        <?php
+                            echo Dropdown::widget([
+                                'id'=> "profile_dropdown",
+                                'items' => [
+                                    ['label' => 'Профиль', 'url' => '/'],
+                                    ['label' => 'Выход', 'url' => '/logout', 'linkOptions' =>
+                                        [
+                                            'data-method' => 'post'
+                                        ]
+                                    ],
+                                ],
+                            ]);
+                    ?>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
