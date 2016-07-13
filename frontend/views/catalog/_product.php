1f4d449e271390f52079b3ba31a2c39851b4f56c
Webmin <webmin@rx24.ru>
2016-07-13 23:31:49 +0500

    ?????? ?? ?????, ???????? ??????? data-pjax="0"

--- frontend/views/catalog/_product.php
+++ frontend/views/catalog/_product.php
@@ -1,13 +1,13 @@
 <div class="item-box col-md-3 col-sm-4 col-xs-6">
     <div class="item-img">
-        <a href="/product/view?id=<?=$model->id?>">
+        <a data-pjax="0" href="/product/view?id=<?=$model->id?>">
             <div class="item-thumb">
                 <img src="<?=$model->photo?>">
             </div>
         </a>
     </div>
     <div class="description">
-        <a href="/product/view?id=<?=$model->id?>">
+        <a data-pjax="0" href="/product/view?id=<?=$model->id?>">
             <h4 class="item-title"><?=$model->name?></h4>
         </a>
         <div class="dop">
@@ -18,7 +18,7 @@
     </div>
     <div class="price">
         <h3 class="item-price pull-left"><?=$model->price?> р.</h3>
-           <a href="/product/view?id=<?=$model->id?>" class="btn btn-default btn-sm pull-right fav">
+           <a data-pjax="0" href="/product/view?id=<?=$model->id?>" class="btn btn-default btn-sm pull-right fav">
             Подробнее
          </a>
     </div>
