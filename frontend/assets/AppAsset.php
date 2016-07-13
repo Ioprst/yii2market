dea371dd3b74e9b84e0ee846c8e36337426d4957
Webmin <webmin@rx24.ru>
2016-07-13 20:41:58 +0500

    ?????????? js ? ?ss

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,11 +13,14 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/main.css',
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'rmrevin\yii\fontawesome\AssetBundle'
     ];
 }
