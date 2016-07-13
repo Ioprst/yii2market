c789c41e965f29b66381f3a959ee0fd1ed04875d
Webmin <webmin@rx24.ru>
2016-07-13 13:37:09 +0500

    ?????????? ?????? ? ????

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -3,9 +3,9 @@
         <?php
             $items = [
                 ['label' => '', 'options' => ['class' => 'header']],
-                ['label' => '<i class="fa fa-users"></i> <span>Категории товара</span>','icon' => 'fa fa-users', 'url' => ['/product-category/index']],
+                ['label' => '<i class="fa fa-users"></i> <span>Категории товара</span>','icon' => 'fa fa-users', 'url' => ['/category/index']],
                 ['label' => '<i class="fa fa-users"></i> <span>Товар</span>','icon' => 'fa fa-users', 'url' => ['/product/index']],
-                ['label' => '<i class="fa fa-users"></i> <span>Опции товара</span>','icon' => 'fa fa-users', 'url' => ['product-option/index']],
+                ['label' => '<i class="fa fa-users"></i> <span>Опции товара</span>','icon' => 'fa fa-users', 'url' => ['option/index']],
             ];
         ?>
         <?= \yii\widgets\Menu::widget(
