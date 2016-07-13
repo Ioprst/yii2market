bb4cac291341ce099999342901da5be9822bf175
Webmin <webmin@rx24.ru>
2016-07-13 23:30:31 +0500

    ???????? ????? ? ???????

--- backend/config/main.php
+++ backend/config/main.php
@@ -9,6 +9,7 @@ $params = array_merge(
 return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
+    'name'=>'Super Market',
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
