173f7a95f443c16136afc5056ef2570374b5c902
Webmin <webmin@rx24.ru>
2016-07-13 20:42:30 +0500

    ?????? ?? ?????? ??????? ? ???????

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -3,6 +3,7 @@
         <?php
             $items = [
                 ['label' => '', 'options' => ['class' => 'header']],
+                ['label' => '<i class="fa fa-users"></i> <span>Журнал заказов</span>','icon' => 'fa fa-users', 'url' => ['/order/index']],
                 ['label' => '<i class="fa fa-users"></i> <span>Категории товара</span>','icon' => 'fa fa-users', 'url' => ['/category/index']],
                 ['label' => '<i class="fa fa-users"></i> <span>Товар</span>','icon' => 'fa fa-users', 'url' => ['/product/index']],
                 ['label' => '<i class="fa fa-users"></i> <span>Опции товара</span>','icon' => 'fa fa-users', 'url' => ['option/index']],
