51b4157e7e9095bab7564318293eaa04c6de2250
Webmin <webmin@rx24.ru>
2016-07-13 23:16:07 +0500

    ?????? ?????? ??????????? ? ??????????? ?? ????? ????????? ?????? ?? ???????

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -46,8 +46,7 @@ AppAsset::register($this);
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/login">Вход</a></li>
-                        <li><a href="/signup">Регистрация</a></li>
+                        <li><a href="/admin">Админкa</a></li>
                     </ul>
                 </div>
             </div>
@@ -78,7 +77,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?=Yii::$app->name?> <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
